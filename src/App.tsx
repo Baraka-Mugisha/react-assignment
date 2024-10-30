edd8f4db2be3d2cc52ad176247fe176f81b3e73f
Baraka-Mugisha <mugishaje@gmail.com>
2024-10-30 11:02:44 +0200

    removed user props on sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,9 @@ import TaskList from "./components/TaskList";
 import "./App.css";
 
 function App() {
-  const users = [
-    {
-      id: 1,
-      name: "User One",
-      avatarUrl: "https://randomuser.me/api/portraits/women/1.jpg",
-      isOnline: true,
-    },
-    {
-      id: 2,
-      name: "User Two",
-      avatarUrl: "https://randomuser.me/api/portraits/men/2.jpg",
-      isOnline: true,
-    },
-    {
-      id: 3,
-      name: "User Three",
-      avatarUrl: "https://randomuser.me/api/portraits/women/3.jpg",
-      isOnline: false,
-    },
-  ];
-
-
   return (
     <div className="flex dark:bg-gray-900 dark:text-white transition-colors">
-      <Sidebar users={users} />
+      <Sidebar />
       <div className="flex-1 flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors">
         <Header />
         <div className="flex flex-1 p-4 space-x-4">
